a69dde0f1be0b2fae9eca7cd8aa2b92a5f9f905a
rolly <huxiaoying19870802@126.com>
2017-04-11 22:15:14 +0800

    ajax

---
+++ ajax/myapp/routes/ajax.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	res.render('ajax', {lmj: 'hxy', a: {b: 1}});
+});
+
+router.get('/api', function(req, res, next) {
+	console.log(req.query)
+	console.log(req.body)
+  res.send({
+  	success: false,
+  	data: 123,
+  	message: ''
+  });
+});
+
+router.post('/api', function(req, res, next) {
+	console.log(req.query)
+	console.log(req.body)
+	
+  res.send({
+  	success: false,
+  	data: 123,
+  	message: ''
+  });
+});
+
+module.exports = router;
